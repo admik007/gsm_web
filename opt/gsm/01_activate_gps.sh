470590c10e0d4064eb4ae5b5a9383288dbdc7271
Tomas KOVAC <office@ztk-comp.sk>
2021-03-20 19:18:50 +0100

    Update 01_activate_gps.sh

--- opt/gsm/01_activate_gps.sh
+++ opt/gsm/01_activate_gps.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-screen -S get_gps -dm python /opt/gsm/get_gps.py
+#stty -F /dev/ttyS0 -echo
+#gpsd /dev/ttyS0 -nb -S 9999 -G -F /var/run/gpsd.sock
+#gpsd /dev/ttyUSB0 -nb -S 9999 -G -F /var/run/gpsd.sock
+
+rfcomm bind /dev/rfcomm0 00:02:76:C8:D9:C8
+gpsd /dev/rfcomm0 -nb -S 9999 -G -F /var/run/gpsd.sock
